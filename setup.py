f63073ce94e2b5608f1eca34d43af92931bbb6ea
Nuri <santosrodriguezazu@gmail.com>
2026-05-03 23:55:38 -0600

    Proyecto

--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@ from setuptools import setup, find_packages
 setup(
     name="cellcounter",
     version="1.0.0",
+    author="Casillas M., Nuñez L., Muñoz V., Santos N.",
+    description="Librería para detección y conteo de células en imágenes microscópicas",
+    long_description="Proyecto de Procesamiento de Imágenes - Ingeniería ROBOTICA",
     packages=find_packages(),
     install_requires=[
         "numpy",
-        "opencv-python",
         "matplotlib",
+        "opencv-python",
+        "scipy"
     ],
+    python_requires='>=3.8',
 )
